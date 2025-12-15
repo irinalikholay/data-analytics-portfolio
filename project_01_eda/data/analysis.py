d81cfeaecda18ee29d9420d23596a249a9363303
irinalikholay <lihollaykaa@gmail.com>
2025-12-16 00:12:04 +0100

    Create first DataFrame

--- project_01_eda/data/analysis.py
+++ project_01_eda/data/analysis.py
@@ -8,3 +8,18 @@ import pandas as pd
 import numpy as np
 
 print("Libraries imported successfully")
+
+
+# Create a simple dataset
+
+data = {
+    "customer_id": [1, 2, 3, 4, 5],
+    "age": [23, 35, 45, 28, 52],
+    "country": ["Germany", "France", "Germany", "Spain", "France"],
+    "purchase_amount": [120, 340, 560, 80, 220]
+
+    }
+
+df = pd.DataFrame(data)
+
+print(df)
