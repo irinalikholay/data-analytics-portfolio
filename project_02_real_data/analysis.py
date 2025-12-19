4a33a28ba84dd784aaf59785b4f4765c8be22f2b
irinalikholay <lihollaykaa@gmail.com>
2025-12-19 23:01:55 +0100

    Initial EDA: load data and inspect structure

---
+++ project_02_real_data/analysis.py
@@ -0,0 +1,19 @@
+import os
+print("CWD:", os.getcwd())
+print("FILES:", os.listdir())
+
+import os 
+print("I AM RUNNING THIS FILE")
+print(os.getcwd())
+
+
+import pandas as pd
+
+# Load dataset 
+df = pd.read_csv("data/sales_data.csv")
+
+print("Dataset loaded successfully")
+print(df.head())
+print(df.info())
+print(df.isnull().sum())
+print(df.describe())
