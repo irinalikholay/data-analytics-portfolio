ac95a5ae92b92debdd560eabef621f42cd979f74
irinalikholay <lihollaykaa@gmail.com>
2025-12-21 21:14:26 +0100

    Remove rows with negative spend

--- project_03_data_cleaning/analysis.py
+++ project_03_data_cleaning/analysis.py
@@ -7,3 +7,16 @@ print(df.info())
 
 df["signup_date"] = pd.to_datetime(df["signup_date"])
 print(df.info())
+
+#Fill missing age with median 
+median_age = df["age"].median()
+df["age"] = df["age"].fillna(median_age)
+
+print("Missing value is age after filling")
+print(df["age"].isnull().sum())
+
+#Remove rows with negative spend
+
+df = df[df["spend"] >= 0 ]
+print("Rows after removing negative spend:")
+print(len(df))
