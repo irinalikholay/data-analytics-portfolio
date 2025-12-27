558a9850c803f87abd736feceef461a767c3893b
irinalikholay <lihollaykaa@gmail.com>
2025-12-27 23:32:12 +0100

    Load and inspect dataset

---
+++ project_05_advanced_analysis/analysis.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# 1. Load data
+df = pd.read_csv("data/sales_customers.csv")
+
+#2. Basic checks 
+print("Dataset shape:")
+print(df.shape)
+
+print("\nFirst 5 rows:")
+print(df.head())
+
+print("\nData types:")
+print(df.dtypes)
+
+print("\nMissing values:")
+print(df.isna().sum())
